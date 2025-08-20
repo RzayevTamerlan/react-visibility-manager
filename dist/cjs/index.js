f1e99ce8a1c791de8a3b34d8ea1f3b11263fcbd2
RzayevTamerlan <tamerlan.rzev@gmail.com>
2025-08-21 01:42:08 +0400

    feat: Optimize for ESM and CJS

---
+++ dist/cjs/index.js
@@ -0,0 +1,97 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.VisibilityTrigger = exports.VisibilityTarget = exports.VisibilityProvider = void 0;
+exports.useVisibility = useVisibility;
+exports.useVisibilityTarget = useVisibilityTarget;
+const jsx_runtime_1 = require("react/jsx-runtime");
+const react_1 = require("react");
+const VisibilityContext = (0, react_1.createContext)(undefined);
+function visibilityReducer(state, action) {
+    switch (action.type) {
+        case 'TOGGLE': {
+            const newValue = !state[action.key];
+            if (action.mode === 'single' && newValue) {
+                return { [action.key]: true };
+            }
+            return {
+                ...state,
+                [action.key]: newValue,
+            };
+        }
+        case 'SET': {
+            if (action.mode === 'single' && action.value) {
+                return { [action.key]: true };
+            }
+            return {
+                ...state,
+                [action.key]: action.value,
+            };
+        }
+        case 'RESET':
+            return action.initial;
+        default:
+            return state;
+    }
+}
+const VisibilityProvider = ({ children, initialState = {}, mode = 'multiple', }) => {
+    const [state, dispatch] = (0, react_1.useReducer)(visibilityReducer, initialState);
+    const toggle = (0, react_1.useCallback)((key) => dispatch({ type: 'TOGGLE', key, mode }), [mode]);
+    const set = (0, react_1.useCallback)((key, value) => dispatch({ type: 'SET', key, value, mode }), [mode]);
+    const reset = (0, react_1.useCallback)((initial) => dispatch({ type: 'RESET', initial }), []);
+    return ((0, jsx_runtime_1.jsx)(VisibilityContext.Provider, { value: { state, toggle, set, reset }, children: children }));
+};
+exports.VisibilityProvider = VisibilityProvider;
+function useVisibility() {
+    const context = (0, react_1.useContext)(VisibilityContext);
+    if (!context) {
+        throw new Error('useVisibility must be used within a VisibilityProvider');
+    }
+    return context;
+}
+const VisibilityTarget = ({ children, targetKey, isOpenPropName = 'isOpen', shouldRender = true, wrapper, ...restProps }) => {
+    const { state } = useVisibility();
+    const isOpen = Boolean(state[targetKey]);
+    const newProps = {
+        ...children.props,
+        ...restProps,
+        [isOpenPropName]: isOpen,
+    };
+    if (!shouldRender && !isOpen) {
+        return null;
+    }
+    const cloned = (0, react_1.cloneElement)(children, newProps);
+    return wrapper ? wrapper(cloned, isOpen) : cloned;
+};
+exports.VisibilityTarget = VisibilityTarget;
+const VisibilityTrigger = ({ children, triggerKey, propName = 'onClick', ...restProps }) => {
+    const { toggle } = useVisibility();
+    if (!children) {
+        throw new Error('VisibilityTrigger requires a child element');
+    }
+    if (!(0, react_1.isValidElement)(children)) {
+        throw new Error('VisibilityTrigger child must be a valid React element');
+    }
+    const existingOnClick = children?.props[propName];
+    const handleClick = (e) => {
+        existingOnClick?.(e);
+        toggle(triggerKey);
+    };
+    const newProps = { ...children?.props, restProps, [propName]: handleClick };
+    return (0, react_1.cloneElement)(children, newProps);
+};
+exports.VisibilityTrigger = VisibilityTrigger;
+function useVisibilityTarget(targetKey) {
+    const { state, toggle: globalToggle, set: globalSet } = useVisibility();
+    const isOpen = Boolean(state[targetKey]);
+    const toggle = (0, react_1.useCallback)(() => {
+        globalToggle(targetKey);
+    }, [targetKey, globalToggle]);
+    const set = (0, react_1.useCallback)((value) => {
+        globalSet(targetKey, value);
+    }, [targetKey, globalSet]);
+    return {
+        isOpen,
+        toggle,
+        set,
+    };
+}
